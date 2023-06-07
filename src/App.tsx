c002de0730a8c1e0e5987273ffb0503e28da6de5
Mount-Book <karaagerappy+nkc@gmail.com>
2023-06-07 16:24:16 +0900

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,162 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// React から　useState フックをインポート
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+// "Todo型" の定義
+type Todo = {
+  // プロパティ value は文字列型
+  value: string;
+  readonly id: number;
+  checked: boolean;
+  removed: boolean;
+};
+
+type Filter = 'all' | 'checked' | 'unchecked' | 'removed';
+
+export const App = () => {
+  // 初期値：空文字列 ''
+  const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
-  
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
-      <h1>電車で櫛使って髪整えるな</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      let id = 1;
-      const todosList = [
-    {
-      id: 1,
-      content: "店を予約する",
-      editing: false,
-    },
-    </>
-  );
-}
+  const [filter, setFilter] = useState<Filter>('all');
 
-export default App;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  // todosステートを更新する関数
+  const handleSubmit = () => {
+    //何も入力されていなかったらリターン
+    if (!text) return;
+
+    // 新しい Todoを作成
+    //明示的に型注釈をつけてオブジェクトの型を限定する
+    const newTodo: Todo = {
+      // textステートの値をvalue プロパティへ
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
+    };
+
+    /**
+     * 更新前のtodosステートを元に
+     * スプレッド構文で展開した要素へ
+     * newTodoを加えた新しい配列でステートを更新
+     **/
+    setTodos((todos) => [newTodo, ...todos]);
+    //フォームへの入力をクリアする
+    setText('');
+  };
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, value: value };
+        }
+        return todo;
+      });
+
+      console.log('=== Original todos ===');
+      todos.map((todo) => {
+        console.log(`id: ${todo.id}, value: ${todo.value}`);
+      });
+      return newTodos;
+    });
+  };
+  const handlecheck = (id: number, checked: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, checked };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
+  const handleRemove = (id: number, removed: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, removed };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
+  const handleSort = (filter: Filter) => {
+    setFilter(filter);
+  };
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case 'all':
+        return !todo.removed;
+      case 'checked':
+        return todo.checked && !todo.removed;
+      case 'unchecked':
+        return !todo.checked && !todo.removed;
+      case 'removed':
+        return todo.removed;
+      default:
+        return todo;
+    }
+  });
+  const handleEmpty = () => {
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };
+  return (
+    <div>
+      <select
+        defaultValue="all"
+        onChange={(e) => handleSort(e.target.value as Filter)}
+      >
+        <option value="all">すべてのタスク</option>
+        <option value="chacked">完了したラスク</option>
+        <option value="unchecked">現在のタスク</option>
+        <option value="removed">ゴミ箱</option>
+      </select>
+      {filter === 'removed' ? (
+        <button
+          onClick={handleEmpty}
+          disabled={todos.filter((todo) => todo.removed).length === 0}
+        >
+          ごみ箱を空にする
+        </button>
+      ) : (
+        filter !== 'checked' && (
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <input type="text" value={text} onChange={(e) => handleChange(e)} />
+            <input type="submit" value="追加" onSubmit={handleSubmit} />
+          </form>
+        )
+      )}
+      <ul>
+        {filteredTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                disabled={todo.removed}
+                checked={todo.checked}
+                onChange={() => handlecheck(todo.id, !todo.checked)}
+              />
+              <input
+                type="text"
+                disabled={todo.checked || todo.removed}
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+              <button onClick={() => handleRemove(todo.id, !todo.removed)}>
+                {todo.removed ? 'Ctrl + Z' : 'でれて'}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
