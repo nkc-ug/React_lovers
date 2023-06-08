c7c46ea0e2aeaa0d802d5018c70bcb8eb2fa4fa4
suzukihaya <hayahayawork@gmail.com>
2023-06-08 12:56:43 +0900

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { App } from './App.tsx';
+import {Api} from './Api.tsx';
 import './index.css';
 
 const root = createRoot(document.getElementById('root') as Element);
@@ -8,5 +9,6 @@ const root = createRoot(document.getElementById('root') as Element);
 root.render(
   <StrictMode>
     <App />
+    <Api />
   </StrictMode>
 );
