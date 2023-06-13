458ade7cbd4fa9c7feb9861ab9bcad86880076fe
Yuma Satake <satous.y@icloud.com>
2023-06-13 14:56:09 +0900

    fix1

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,71 +1,56 @@
-import { useState, useEffect } from 'react';
-import title from './App.tsx';
-import { Axios } from 'axios';
-const YOUTUBE_SERACH_API_URI = 'https://www.googleapis.com/youtube/v3/search?';
+import { useState, useEffect, FC } from 'react';
+const YOUTUBE_SEARCH_API_URI = 'https://www.googleapis.com/youtube/v3/search?';
 const API_KEY = 'AIzaSyDTCQ6lNbEM0JPm83EAA86slppwCVltVQY';
 
-const axios = new Axios({
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  responseType: 'json',
-  transformRequest: [
-    function transformRequest(data) {
-      return JSON.stringify(data);
-    },
-  ],
-  transformResponse: [
-    function transformResponse(data) {
-      return JSON.parse(data);
-    },
-  ],
-});
+type Props = {
+  searchWord: string;
+};
 
-export const Api = () => {
-  const [videoId] = useState('');
+export const Api: FC<Props> = (props) => {
+  const [videoId, setVideoId] = useState('');
   useEffect(() => {
     // クエリ文字列を定義する
     const params = {
       key: API_KEY,
-      q: title, // 検索キーワード
+      q: props.searchWord, // 検索キーワード
       type: 'video', // video,channel,playlistから選択できる
       maxResults: '1',
       order: 'viewCount',
     };
 
-    axios.get(YOUTUBE_SERACH_API_URI, { params }).then((res) => {
-      console.log(res.data);
-    });
+    const queryParams = new URLSearchParams(params);
+    const url = `${YOUTUBE_SEARCH_API_URI}?${queryParams}`;
 
-    // const queryParams = new URLSearchParams(params);
+    // APIをコールする
+    fetch(url)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log('API success:', result);
 
-    // // APIをコールする
-    // fetch(YOUTUBE_SERACH_API_URI + queryParams)
-    //   .then((res) => res.json())
-    //   .then(
-    //     (result) => {
-    //       console.log('API success:', result);
-
-    //       if (result.items && result.items.length !== 0) {
-    //         const firstItem = result.items[0];
-    //         setVideoId(firstItem.id.videoId);
-    //       }
-    //     },
-    //     (error) => {
-    //       console.error(error);
-    //     }
-    //   );
-  }, []);
+          if (result.items && result.items.length !== 0) {
+            const firstItem = result.items[0];
+            setVideoId(firstItem.id.videoId);
+          }
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+  }, [props.searchWord]);
 
   return (
-    <iframe
-      id="player"
-      width="640"
-      height="360"
-      src={'https://www.youtube.com/embed/' + videoId}
-      frameBorder="0"
-      allowFullScreen
-    />
+    <>
+      <div>検索文字列：{props.searchWord}</div>
+      <iframe
+        id="player"
+        width="640"
+        height="360"
+        src={'https://www.youtube.com/embed/' + videoId}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </>
   );
 };
 
