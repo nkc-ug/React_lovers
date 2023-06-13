b4b4e35eecbf3cb79b774b610d59c617b60b7f5c
Yuma Satake <satous.y@icloud.com>
2023-06-13 13:47:04 +0900

    fix: axiosで実装に変更

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,38 +1,60 @@
-import { useState, useEffect } from "react";
-import title from "./App.tsx";
-const YOUTUBE_SERACH_API_URI = "https://www.googleapis.com/youtube/v3/search?";
-const API_KEY = "AIzaSyDTCQ6lNbEM0JPm83EAA86slppwCVltVQY";
+import { useState, useEffect } from 'react';
+import title from './App.tsx';
+import { Axios } from 'axios';
+const YOUTUBE_SERACH_API_URI = 'https://www.googleapis.com/youtube/v3/search?';
+const API_KEY = 'AIzaSyDTCQ6lNbEM0JPm83EAA86slppwCVltVQY';
+
+const axios = new Axios({
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  responseType: 'json',
+  transformRequest: [
+    function transformRequest(data) {
+      return JSON.stringify(data);
+    },
+  ],
+  transformResponse: [
+    function transformResponse(data) {
+      return JSON.parse(data);
+    },
+  ],
+});
 
 export const Api = () => {
-  const [videoId, setVideoId] = useState("");
+  const [videoId] = useState('');
   useEffect(() => {
     // クエリ文字列を定義する
     const params = {
       key: API_KEY,
-      q:  title, // 検索キーワード
-      type: "video", // video,channel,playlistから選択できる
-      maxResults: "1",
-      order: "viewCount", 
+      q: title, // 検索キーワード
+      type: 'video', // video,channel,playlistから選択できる
+      maxResults: '1',
+      order: 'viewCount',
     };
 
-    const queryParams = new URLSearchParams(params);
+    axios.get(YOUTUBE_SERACH_API_URI, { params }).then((res) => {
+      console.log(res.data);
+    });
 
-    // APIをコールする
-    fetch(YOUTUBE_SERACH_API_URI + queryParams)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log("API success:", result);
+    // const queryParams = new URLSearchParams(params);
 
-          if (result.items && result.items.length !== 0) {
-            const firstItem = result.items[0];
-            setVideoId(firstItem.id.videoId);
-          }
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
+    // // APIをコールする
+    // fetch(YOUTUBE_SERACH_API_URI + queryParams)
+    //   .then((res) => res.json())
+    //   .then(
+    //     (result) => {
+    //       console.log('API success:', result);
+
+    //       if (result.items && result.items.length !== 0) {
+    //         const firstItem = result.items[0];
+    //         setVideoId(firstItem.id.videoId);
+    //       }
+    //     },
+    //     (error) => {
+    //       console.error(error);
+    //     }
+    //   );
   }, []);
 
   return (
@@ -40,7 +62,7 @@ export const Api = () => {
       id="player"
       width="640"
       height="360"
-      src={"https://www.youtube.com/embed/" + videoId}
+      src={'https://www.youtube.com/embed/' + videoId}
       frameBorder="0"
       allowFullScreen
     />
