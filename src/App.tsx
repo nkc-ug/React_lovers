34445b93ccbd9fe3778392b8a6218d78dd6caec6
MinamizonoEno <a02142211@gmail.com>
2023-06-20 11:41:49 +0900

    このminamiってファイルなんだっけ…

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
 import { Box, Typography, Grid, Button, TextField, AppBar, Toolbar } from '@mui/material';
-
-interface BoxData {
-  id: number;
-  first: string;
-  second: string;
-  third: string;
-  fourth: string;
-}
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   const [isEditing, setIsEditing] = useState(false);
+  const [isDel, setIsDel] = useState(false);
   const [first, setFirst] = useState('');
   const [second, setSecond] = useState('');
   const [third, setThird] = useState('');
   const [fourth, setFourth] = useState('');
   const [boxes, setBoxes] = useState<BoxData[]>([]);
 
+  interface BoxData {
+    id: number;
+    first: string;
+    second: string;
+    third: string;
+    fourth: string;
+  }
+
   const handleCompletion = () => {
     const newBox: BoxData = {
-      id: boxes.length + 1,
+      id: boxes.length,
       first,
       second,
       third,
@@ -33,7 +39,7 @@ function App() {
     setThird('');
     setFourth('');
   };
-
+  //追加と削除ボタンの管理
   const AddButton = () => {
     const handleClick = () => {
       setIsEditing(true);
@@ -45,17 +51,42 @@ function App() {
 
     return isEditing ? (
       <div>
-        <Button color="inherit" onClick={stopEditingHandler}>
-          Cancel
-        </Button>
+        <IconButton color="inherit" onClick={stopEditingHandler}>
+          <CloseIcon />
+        </IconButton>
       </div>
     ) : (
-      <Button color="inherit" onClick={handleClick}>
-        Add
-      </Button>
+      <IconButton color="inherit" onClick={handleClick}>
+        <AddIcon />
+      </IconButton>
     );
   };
+  const DelButton = () => {
+    const handleClick = () => {
+      setIsDel(true);
+    };
 
+    const stopEditingHandler = () => {
+      setIsDel(false);
+    };
+
+    return isDel ? (
+      <div>
+        <IconButton color="inherit" onClick={stopEditingHandler}>
+          <CloseIcon />
+        </IconButton>
+      </div>
+    ) : (
+      <IconButton color="inherit" onClick={handleClick}>
+        <DeleteIcon />
+      </IconButton>
+    );
+  };
+  //デリートをおすと配列内の要素を空にする
+  const handleDelete = (id: number) => {
+    setBoxes((prevBoxes) => prevBoxes.filter((box) => box.id !== id));
+  };
+  //本処理
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -71,12 +102,15 @@ function App() {
                 </Typography>
               </Grid>
               <Grid item xs="auto">
-                <Button color="inherit">Delete</Button>
+                <DelButton />
               </Grid>
             </Grid>
           </Toolbar>
         </AppBar>
       </Box>
+      {
+        //入力フォーム
+      }
       <div>
         {isEditing ? (
           <Box
@@ -148,32 +182,110 @@ function App() {
           ''
         )}
       </div>
+      {
+        //生成されたリスト
+      }
       <div>
-        {boxes.map((box) => (
-          <Box
-            key={box.id}
-            sx={{
-              bgcolor: 'background.paper',
-              boxShadow: 1,
-              borderRadius: 2,
-              p: 2,
-              mt: 4,
-            }}
-          >
-            <Typography variant="body1" component="div">
-              {box.first}
-            </Typography>
-            <Typography variant="body1" component="div">
-              {box.second}
-            </Typography>
-            <Typography variant="body1" component="div">
-              {box.third}
-            </Typography>
-            <Typography variant="body1" component="div">
-              {box.fourth}
-            </Typography>
-          </Box>
-        ))}
+        <Grid container spacing={2}>
+          {boxes.map((box) => (
+            <Grid item xs={12} md={6} lg={4} key={box.id}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  bgcolor: 'background.paper',
+                  boxShadow: 2,
+                  borderRadius: 2,
+                  p: 2,
+                  mt: 4,
+                }}
+              >
+                <Grid container direction="column" alignItems="stretch" spacing={2}>
+                  <Grid item>
+                    {box.first ? (
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          bgcolor: 'background.paper',
+                        }}
+                      >
+                        <Checkbox edge="start" tabIndex={-1} disableRipple />
+                        <Typography variant="body1" component="div">
+                          {box.first}
+                        </Typography>
+                      </Box>
+                    ) : (
+                      ''
+                    )}
+                  </Grid>
+                  <Grid item>
+                    {box.second ? (
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          bgcolor: 'background.paper',
+                        }}
+                      >
+                        <Checkbox edge="start" tabIndex={-1} disableRipple />
+                        <Typography variant="body1" component="div">
+                          {box.second}
+                        </Typography>
+                      </Box>
+                    ) : (
+                      ''
+                    )}
+                  </Grid>
+                  <Grid item>
+                    {box.third ? (
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          bgcolor: 'background.paper',
+                        }}
+                      >
+                        <Checkbox edge="start" tabIndex={-1} disableRipple />
+                        <Typography variant="body1" component="div">
+                          {box.third}
+                        </Typography>
+                      </Box>
+                    ) : (
+                      ''
+                    )}
+                  </Grid>
+                  <Grid item>
+                    {box.fourth ? (
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          bgcolor: 'background.paper',
+                        }}
+                      >
+                        <Checkbox edge="start" tabIndex={-1} disableRipple />
+                        <Typography variant="body1" component="div">
+                          {box.fourth}
+                        </Typography>
+                      </Box>
+                    ) : (
+                      ''
+                    )}
+                  </Grid>
+                </Grid>
+                <Grid container justifyContent="flex-end" alignItems="center">
+                  {isDel ? (
+                    <IconButton color="error" onClick={() => handleDelete(box.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  ) : (
+                    ''
+                  )}
+                </Grid>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </div>
   );
